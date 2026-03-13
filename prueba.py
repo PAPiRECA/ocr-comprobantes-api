f2cbaefeb657c562574078316904ad6475bc96bd
PAPiRECA <enzorecalde2006@gmail.com>
2026-03-13 13:42:25 +0000

    actualiza OCR

--- prueba.py
+++ prueba.py
@@ -49,15 +49,25 @@ def extract_fields(texto: str) -> dict:
     }
 
     # ID transacción
-    id_match = re.search(r"NRO\.\s*TRANSACCION[:\s]*([0-9]{6,})", texto, re.IGNORECASE)
+    id_match = re.search(
+        r"(?:ID\s*Transacci[oó]n|Id\s*T?Transaccion|Id\s*Transaccion|d\s*Transaccion)[:\s]*([0-9]{6,})",
+        texto,
+        re.IGNORECASE
+    )
+
     if not id_match:
         id_match = re.search(
-            r"(?:ID\s*Transacci[oó]n|Id\s*T?Transaccion|Id\s*Transaccion|d\s*Transaccion)[:\s]*([0-9]{6,})",
+            r"ATM\s*Transacci[oó]n\s*ID[:\s]*([0-9]{6,})",
             texto,
             re.IGNORECASE
         )
+
     if not id_match:
-        id_match = re.search(r"ATM\s*Transacci[oó]n\s*ID[:\s]*([0-9]{6,})", texto, re.IGNORECASE)
+        id_match = re.search(
+            r"NRO\.\s*TRANSACCION[:\s]*([0-9]{6,})",
+            texto,
+            re.IGNORECASE
+        )
 
     if id_match:
         fields["id_transaccion"] = id_match.group(1)
